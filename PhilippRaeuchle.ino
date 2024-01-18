4db9b81735b2a5683843f36b3b3093b6ccce9880
LukachoLombardi <spamherrre@gmail.com>
2024-01-18 10:53:26 +0100

    DEV: Implement more states

--- PhilippRaeuchle.ino
+++ PhilippRaeuchle.ino
@@ -271,30 +271,85 @@ void driveSizeUnits(float units) {
 class : public LAS::Callable {
 public:
   void run() override {
+    if (VehicleRotation::isRotationActive()) {
+      return;
+    }
     Sensors::readTOFMMs();
-    if (Sensors::tof_measure_down.RangeMilliMeter >= MAX_TABLE_DISTANCE) {
-      logger.printline("end of table reached. TRANSFER mode start.");
+    if (Sensors::tof_measure_down.RangeMilliMeter >= MAX_TABLE_DISTANCE && Sensors::tof_measure_down.RangeStatus != 4) {
+      logger.printline("end of table reached.");
       pauseDriving();
-      if(state == DRIVE) {
-        state = TRANSFER;
+      if (state == DRIVE) {
+        setState(TRANSFER);
       }
-      if(state == AVOID) {
-        //implement special logic here
+      if (state == AVOID_ENTRY || state == AVOID_EXIT) {
+        rotateVehicleByAsync(1);
+        setState(DRIVE);
+        directionR = !directionR;
+      }
+      if (state == TRANSFER) {
+        logger.printline("Table Finished!");
+        taskPtr->isActive = false;
       }
       return;
     }
-    logger.printline("running drive routine...");
-    int fw_dist = Sensors::tof_measure_fw_low.RangeMilliMeter;
     if (state == DRIVE) {
-      if (Sensors::tof_measure_fw_low.RangeStatus == 4 || (Sensors::tof_measure_fw_low.RangeStatus != 4 && fw_dist > SAFETY_DISTANCE)) {
+      if ((Sensors::tof_measure_fw_low.RangeStatus != 4 && Sensors::tof_measure_fw_low.RangeMilliMeter >= SAFETY_DISTANCE)) {
         logger.printline("no obstacle detected", "debug");
         ResumeDriving();
       } else {
         logger.printline("obstacle detected! Starting AVOID behaviour");
-        state = AVOID;
+        setState(AVOID_ENTRY);
         pauseDriving();
+        rotateVehicleByAsync(3 / 2);
+      }
+    } else if (state == AVOID_ENTRY) {
+      if (Sensors::tof_measure_left.RangeStatus != 4 && Sensors::tof_measure_left.RangeMilliMeter >= DETECTION_DISTANCE) {
+        resumeDriving();
+      } else {
+        pauseDriving();
+        lastLDist = -1;
+        setState(AVOID_EXIT);
+      }
+    } else if (state == AVOID_EXIT) {
+      if (lastLDist == -1 || (Sensors::tof_measure_left.RangeStatus != 4 && abs(Sensors::tof_measure_left.RangeMilliMeter - lastLDist) <= UNITY_DISTANCE)) {
+        resumeDriving();
+      } else {
+        pauseDriving();
+        rotateVehicleByAsync(1 / 4);
+        avoidStage++;
+        if (avoidStage <= 3) {
+          setState(AVOID_ENTRY);
+        } else {
+          avoidStage = 0;
+          if (directionR) {
+            rotateVehicleToAsync(0);
+          } else {
+            rotateVehicleToAsync(1);
+          }
+          setState(DRIVE);
+        }
+        return;
+      }
+      lastLDist = -1;  // might be fucky, test!
+      if (Sensors::tof_measure_left.RangeStatus != 4) {
+        lastLDist = Sensors::tof_measure_left.RangeMilliMeter;
       }
     }
+  }
+private:
+  enum NavState { DRIVE = 0,
+                  AVOID_ENTRY = 1,
+                  AVOID_EXIT = 2,
+                  COLLECT = 3,
+                  TRANSFER = 4,
+                  FINISH = 5 };
+  int avoidStage = 0;
+  void setState(NavState state) {
+    this->state = state;
+    char buffer[BUFFER_SIZE];
+    strcpy(buffer, "switched state to: ");
+    strcat("buffer", int(state));
+    logger.printline(buffer);
   }
   void pauseDriving() {
     logger.printline("setting steppers inactive", "debug");
@@ -306,15 +361,8 @@ public:
     rotatorLeft->resume();
     rotatorRight->resume();
   }
-private:
-  enum Direction { FW,
-                   BW };
-  Direction direction = FW;
-  enum NavState { DRIVE,
-                  AVOID,
-                  COLLECT,
-                  TRANSFER,
-                  FINISH };
+  int lastLDist = 0;
+  bool directionR = true;
   NavState state = DRIVE;
   StepperRotator *rotatorLeft = Navigation::scheduleConstantLeftRotatorAsync();
   StepperRotator *rotatorRight = Navigation::scheduleConstantRightRotatorAsync();
